9d998a7db08288b60fed0e984975d16d809f59de
christinaholt <christina.holt@noaa.gov>
2016-03-02 23:43:01 +0000

    Now plots a field one a map. Added a dictionary.

--- maps.py
+++ maps.py
@@ -6,18 +6,44 @@ import matplotlib.pyplot as plt
 class general_map:
     def __init__(self,fig_title):
         self._fig_title=fig_title
-    
+        set_fig_params()
+   
+
+    def set_fig_params():
+        self.params = {
+                 'hgt': {'250': {'int':  12, 'min': 960, 'max': 1110}, 
+                         '500': {'int':   6, 'min': 498, 'max':  600},
+                         '700': {'int':   6, 'min': 252, 'max':  324},
+                         '850': {'int':   6, 'min': 102, 'max':  178},
+                         'unit': 'dm'},
+                 'tmp': {'500': {'int': 2.5, 'min': -75, 'max':  15},
+                         '700': {'int': 2.5, 'min': -45, 'max':  45},
+                         '850': {'int': 2.5, 'min': -45, 'max':  45},
+                         'unit': 'C'},
+                 'vort':{'500': {'int':   2, 'min': -20, 'max':  20},
+                         'unit': '10e-5 s-1'}, 
+                 'vvel':{'700': {'int':   5, 'min': -23, 'max':  38},
+                         'unit': '-Pa/s*10'}, 
+                 'wind':{'250': {'int':  20, 'min':   0, 'max': 220}, 
+                         'unit': 'kt'},
+                 'rh'  :{'850': {'int':  10, 'min':   0, 'max': 105},
+                         'unit': '%'} 
+                 }
+
+
     def draw_map(self):
                # Make figure
         lon=self.lon
         lat=self.lat
+        fp=self.params 
         m=self.m
         plt.figure(figsize=(12,8))
-        #m =Basemap(projection='stere',lon_0=center[0],lat_0=center[1],lat_ts=center[2],\
-        #            llcrnrlat=center[3],urcrnrlat=center[4],\
-        #            llcrnrlon=center[5],urcrnrlon=center[6],\
-        #            rsphere=6371200.,resolution='l',area_thresh=10000)
         x, y = m(lon,lat)
+        min=fp[self.myvar][str(sefl.level)]['min']
+        max=fp[self.myvar][str(sefl.level)]['max']
+        int=fp[self.myvar][str(sefl.level)]['int']
+
+        print 
 
         # Draw political boundaries
         m.drawcoastlines()
@@ -31,11 +57,17 @@ class general_map:
         m.drawparallels(parallels,labels=[1,0,0,0],fontsize=10)
         meridians = np.arange(0.,360.,20.)
         m.drawmeridians(meridians,labels=[0,0,0,1],fontsize=10)
-        data=self.field.values
+        data=self.field.values/10.
 #        lons, lats = np.meshgrid(self.lon, self.lat)
-        x, y = m(*np.meshgrid(self.lon, self.lat))
-        cs = plt.pcolormesh(x,y,data,shading='flat',cmap=plt.cm.jet)
-        plt.colorbar(cs,orientation='vertical')
+#        x, y = m(*np.meshgrid(lon, lat))
+#        x, y = m(self.lon, self.lat)
+        clevs = np.arange(min,max,int)
+        cs = m.contourf(x,y,data,clevs,cmap=plt.cm.jet)
+#        cs.set_clim()
+        cbar=plt.colorbar(cs,orientation='horizontal')
+#        cbar.set_clim(9000,11000)
+        cbar.set_ticks(np.arange(900,1200,24))
+        cbar.set_ticklabels(np.arange(900,1200,24))
         plt.title('Example 2')
 #        plt.show()
 
@@ -72,24 +104,26 @@ class general_map:
 
 
 class global_map(general_map):
-    def __init__(self,field,lat,lon,date,hour,myvar,fig_title,area_flag=None,ncep_grid=None,resolution=None):
+    def __init__(self,field,lat,lon,date,hour,myvar,level,fig_title,area_flag=None,ncep_grid=None,resolution=None):
         #super(global_map).__init__(self,fig_title)
 
         self.field=field
         self.lat, self.lon = field.latlons()
+        self.myvar=myvar
+        self.level=level
         if area_flag == None or area_flag == 'glob':
-            self.m=Basemap(projection='mill',lat_ts=10,llcrnrlon=-180,
-                urcrnrlon=180,llcrnrlat=-90,urcrnrlat=90,
+            self.m=Basemap(projection='mill',lat_ts=10,llcrnrlon=0,
+                urcrnrlon=357.5,llcrnrlat=-90,urcrnrlat=90,
                 resolution='c')
 
         elif area_flag == 'nh':
-            self.m=Basemap(projection='mill',lat_ts=10,llcrnrlon=-180,
-                urcrnrlon=180,llcrnrlat=0,urcrnrlat=90,
+            self.m=Basemap(projection='mill',lat_ts=10,llcrnrlon=0,
+                urcrnrlon=357.5,llcrnrlat=0,urcrnrlat=90,
                 resolution='c')
 
         elif area_flag == 'sh':
-            self.m=Basemap(projection='mill',lat_ts=10,llcrnrlon=-180,
-                urcrnrlon=180,llcrnrlat=-90,urcrnrlat=0,
+            self.m=Basemap(projection='mill',lat_ts=10,llcrnrlon=0,
+                urcrnrlon=357.5,llcrnrlat=-90,urcrnrlat=0,
                 resolution='c')
 
 class conus_map(general_map):
